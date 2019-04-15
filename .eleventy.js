dbb9bfe47a76d4088266db6f0f8a672336dcdf75
Bryan King <bpking15@gmail.com>
2019-04-15 16:53:37 -0400

    filter for formatting dates

---
+++ .eleventy.js
@@ -0,0 +1,31 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("displayDate", function(date) {
+    var month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    var ordinal = {
+      1 : "st",
+      2 : "nd",
+      3 : "rd",
+      21 : "st",
+      22 : "nd",
+      23 : "rd",
+      31 : "st"
+    };    
+    let d = new Date(date);
+    return `${month[d.getMonth()]} ${d.getDate()}${(ordinal[d.getDate()] || "th")}, ${d.getUTCFullYear()}`;
+  });
+
+};
